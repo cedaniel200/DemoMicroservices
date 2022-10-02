12f756d823f72832b366f08c28869030ff3606b8
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-10-02 14:49:01 -0500

    Test: Add test

---
+++ gateway/src/test/java/com/example/demo/GatewayApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+class GatewayApplicationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
