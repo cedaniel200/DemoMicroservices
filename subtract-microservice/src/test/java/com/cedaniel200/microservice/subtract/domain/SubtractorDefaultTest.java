355da43dc45fe668df298b7fb7989abc90248c61
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-10-02 14:39:45 -0500

    Refactor: Rename SubtractControllerTest to SubtractorDefaultTest

--- subtract-microservice/src/test/java/com/cedaniel200/microservice/subtract/domain/SubtractorDefaultTest.java
+++ subtract-microservice/src/test/java/com/cedaniel200/microservice/subtract/domain/SubtractorDefaultTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SubtractControllerTest {
+class SubtractorDefaultTest {
     private Subtractor subtractor;
 
     @BeforeEach
